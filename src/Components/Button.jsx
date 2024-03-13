1d2d3ad6c053adf7c06e5b452fc4be885bae1385
Amit <amitkm.4211@gmail.com>
2024-03-13 23:32:18 +0530

    Super Quality & Services Section, Done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -5,7 +5,9 @@ const Button = ({ label, iconURL }) => {
 		<button className="bg-coral-red rounded-full text-white border border-coral-red text-lg leading-none px-7 py-4 font-montserrat flex justify-center items-center gap-2">
 			{/* <div>ab</div> */}
 			{label}
-			<img src={iconURL} alt="Button Image" className="ml-2 rounded-full w-5 h-5" />
+			{iconURL && (
+				<img src={iconURL} alt="Button Image" className="ml-2 rounded-full w-5 h-5" />
+			)}
 		</button>
 	);
 };
--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,11 +1,15 @@
 // import React from 'react';
 
+import ServiceCard from '../Components/ServiceCard';
+import { services } from '../constants';
+
 const Services = () => {
 	return (
-		<>
-			<h1>Services</h1>
-			<div>Services</div>
-		</>
+		<section className="flex max-container flex-wrap justify-center gap-9">
+			{services.map((service) => (
+				<ServiceCard key={service.label} {...service} />
+			))}
+		</section>
 	);
 };
 
