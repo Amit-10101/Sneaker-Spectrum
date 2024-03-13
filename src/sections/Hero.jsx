09931e4e6b257fb2a003d8cb9ff682d0ebfc5b4f
Amit <amitkm.4211@gmail.com>
2024-03-14 01:27:59 +0530

    Fixed Minor CSS Issues

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -15,7 +15,7 @@ const Hero = () => {
 		>
 			<div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
 				<p className="font-montserrat text-xl text-coral-red">Our Summer Collections</p>
-				<h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+				<h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
 					<span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
 						The New Arrival
 					</span>
@@ -53,8 +53,8 @@ const Hero = () => {
 					className="object-contain relative z-10"
 				/>
 				<div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
-					{shoes.map((shoe) => (
-						<div key={shoe.label}>
+					{shoes.map((shoe, index) => (
+						<div key={index}>
 							<ShoeCard
 								imgURL={shoe}
 								changeBigShoe={(shoe) => setBigShoeImg(shoe)}
